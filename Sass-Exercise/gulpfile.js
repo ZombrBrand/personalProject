35c933324444e6d2475ebcf582dd60fe534afa17
lzj222312 <645383348@qq.com>
2017-08-03 03:25:01 +0800

    SASS

---
+++ Sass-Exercise/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const browseSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const reload = browseSync.reload;
+
+gulp.task('sass',function(){
+    return gulp.src('./sass/index.scss')
+               .pipe(sass({
+                   outputStyle:'expanded'
+               }).on('error',sass.logError))
+               .pipe(gulp.dest('./css'))
+               .pipe(reload({stream:true}));       
+})
+
+gulp.task("default",function(){
+    browseSync.init({
+        server:{
+            baseDir:'./'
+        },
+        port:'1222'
+    });
+    gulp.watch('./sass/*.scss',['sass'])
+})

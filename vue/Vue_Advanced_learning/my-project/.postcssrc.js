07301b110944941db3785ec1ebc40f21425d0b50
lzj222312 <645383348@qq.com>
2017-08-26 03:36:50 +0800

    Vue_Advanced_learning

---
+++ vue/Vue_Advanced_learning/my-project/.postcssrc.js
@@ -0,0 +1,9 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+// http://hao.jobbole.com/postcss/  中文介绍postcss是基本使用
+
+module.exports = {
+  "plugins": {
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  }
+}
